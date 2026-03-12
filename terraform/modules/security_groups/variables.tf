97f3ba4b406d37e325a18906c051b8d68af09d85
maheshkotlacv-sys <mahesh.kotla.cv@gmail.com>
2026-03-11 22:04:19 -0500

    Update variables.tf

--- terraform/modules/security_groups/variables.tf
+++ terraform/modules/security_groups/variables.tf
@@ -1,4 +1,14 @@
-variable "name_prefix"        { type = string }
-variable "vpc_id"             { type = string }
-variable "mgmt_allowed_cidrs" { type = list(string); default = [] }
-variable "tags"               { type = map(string); default = {} }
+variable "name_prefix" {
+  type = string
+}
+variable "vpc_id" {
+  type = string
+}
+variable "mgmt_allowed_cidrs" {
+  type    = list(string)
+  default = []
+}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
