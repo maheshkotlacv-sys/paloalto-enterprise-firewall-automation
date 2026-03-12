8dc0c87debde24f7cc09e82aaddf3013f839ee58
maheshkotlacv-sys <mahesh.kotla.cv@gmail.com>
2026-03-11 22:03:07 -0500

    Update variables.tf

--- terraform/modules/bootstrap_s3/variables.tf
+++ terraform/modules/bootstrap_s3/variables.tf
@@ -1,4 +1,14 @@
-variable "name_prefix" { type = string }
-variable "bucket_name" { type = string }
-variable "init_cfg"    { type = string; description = "Rendered init-cfg.txt content" }
-variable "tags"        { type = map(string); default = {} }
+variable "name_prefix" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+variable "init_cfg" {
+  type        = string
+  description = "rendered init-cfg.txt content"
+}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
